69f8d7955a79e615c6ce9ee6a43bd95a8a88993d
Vladislav Nozhkin <VNozhkin@exolve.ru>
2024-07-14 14:04:24 +0300

    added Register handler

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -20,7 +20,7 @@ type Auth interface {
 		ctx context.Context,
 		email string,
 		password string,
-	) (userId int64, err error)
+	) (userID int64, err error)
 
 	IsAdmin(
 		ctx context.Context,
@@ -64,7 +64,20 @@ func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
-	panic("implement me")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	// Implement register via auth service
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
 
 func (s *serverAPI) IsAdmin(
@@ -85,6 +98,18 @@ func validateLogin(req *ssov1.LoginRequest) error {
 
 	if req.GetAppId() == emptyValue {
 		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *ssov1.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
 	}
 
 	return nil
