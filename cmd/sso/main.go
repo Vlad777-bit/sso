4a3ca544079a971e483c4d41fe10a74a798e0bb2
Vladislav Nozhkin <VNozhkin@exolve.ru>
2024-07-14 12:02:43 +0300

    added logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
 
-	fmt.Println(cfg)
-
-	// TODO: инициализация логгера
+	log.Info("starting application", slog.Any("config", cfg))
 
 	// TODO: инициализация приложения (app)
 
 	// TODO: запуск grpc-сервер
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	options := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := options.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
