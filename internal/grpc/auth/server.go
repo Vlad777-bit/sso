212845e41ad627d70c142499afd5365f55d277e6
Vladislav Nozhkin <VNozhkin@exolve.ru>
2024-07-19 17:27:08 +0300

    updated

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -2,10 +2,13 @@ package auth
 
 import (
 	"context"
+	"errors"
 	ssov1 "github.com/Vlad777-bit/protos/gen/go/sso"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"sso/internal/services/auth"
+	"sso/internal/storage"
 )
 
 type Auth interface {
@@ -51,7 +54,10 @@ func (s *serverAPI) Login(
 	// Implement login via auth service
 	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), req.GetAppId())
 	if err != nil {
-		// TODO: ...
+		if errors.Is(err, auth.ErrInvalidCredentials) {
+			return nil, status.Error(codes.InvalidArgument, "invalid email or password")
+		}
+
 		return nil, status.Error(codes.Internal, "internal error")
 	}
 
@@ -71,7 +77,10 @@ func (s *serverAPI) Register(
 	// Implement register via auth service
 	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
 	if err != nil {
-		// TODO: ...
+		if errors.Is(err, auth.ErrUserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+
 		return nil, status.Error(codes.Internal, "internal error")
 	}
 
@@ -91,7 +100,10 @@ func (s *serverAPI) IsAdmin(
 	// Implement isAdmin via auth service
 	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
 	if err != nil {
-		// TODO: ...
+		if errors.Is(err, storage.ErrUserNotFound) {
+			return nil, status.Error(codes.NotFound, "user not found")
+		}
+
 		return nil, status.Error(codes.Internal, "internal error")
 	}
 
